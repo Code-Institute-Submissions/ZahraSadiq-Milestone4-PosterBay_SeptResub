ea8771423c83feeb7b40063e6451792d7790d3b6
ZahraSadiq <zsadiq27@gmail.com>
2021-03-24 21:07:41 +0000

    Added products app, fixtures for products & categories, and created models

---
+++ products/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Categories(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Category'
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(
+        max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Product(models.Model):
+    categories = models.ForeignKey(
+        "Categories", null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(
+        max_length=250, null=True, blank=True)
+    name = models.CharField(max_length=300)
+    description = models.TextField()
+    side_note = models.TextField()
+    artist = models.CharField(max_length=250)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image_url = models.URLField(
+        max_length=2000, null=True, blank=True)
+    image = models.ImageField(
+        null=True, blank=True)
+
+    def __str__(self):
+        return self.name
