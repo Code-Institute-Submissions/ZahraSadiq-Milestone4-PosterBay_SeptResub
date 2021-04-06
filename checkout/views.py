f4a328cd54d6b508089321b1ea4687a39a6a0a86
ZahraSadiq <zsadiq27@gmail.com>
2021-04-06 21:46:40 +0000

    Styled checkout_success page

--- checkout/views.py
+++ checkout/views.py
@@ -94,9 +94,8 @@ def checkout_success(request, order_number):
     """
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
-    messages.success(request, f'Great, Your order has been placed! \
-        Your order number is {order_number}. A confirmation email \
-             will be sent to {order.email}.')
+    messages.success(request, f'Your order has been placed! \
+        A confirmation email will be sent to {order.email}.')
 
     # once order is placed delete session
     if 'bag' in request.session:
