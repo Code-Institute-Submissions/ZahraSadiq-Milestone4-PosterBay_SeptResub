304e701c03845cee5a0bad4f0cc806326eb2a8aa
ZahraSadiq <zsadiq27@gmail.com>
2021-04-08 17:42:47 +0000

    Updated chckout form

--- checkout/forms.py
+++ checkout/forms.py
@@ -5,39 +5,34 @@ from .models import Order
 class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
-        fields = (
-            'first_name', 'last_name', 'email', 'phone_number',
-            'country', 'postcode', 'city', 'streetaddress',
-            'house_number',)
+        fields = ('first_name', 'last_name', 'email', 'phone_number',
+                  'country', 'postcode', 'city', 'streetaddress',
+                  'house_number',)
 
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'first_name': 'First Name',
+            'last_name': 'Last Name',
+            'email': 'Email',
+            'phone_number': 'Phone Number',
+            'postcode': 'Postcode',
+            'city': 'City',
+            'streetaddress': 'Street Address',
+            'house_number': 'House Number',
+        }
 
-def __init__(self, *args, **kwargs):
-    """
-    Add placeholders and classes and remove auto-generated
-    labels and set autofocus on first field
-    """
-    super().__init__(*args, **kwargs)
-    placeholders = {
-        'first_name': 'First Name',
-        'last_name': 'Last Name',
-        'email': 'Email',
-        'phone_number': 'Phone Number',
-        'country': 'Country',
-        'postcode': 'Postcode',
-        'city': 'City',
-        'streetaddress': 'Street Address',
-        'house_number': 'House Number',
-    }
-
-    """
-    Sets the cursor in the name field when page is loaded
-    """
-    self.fields['first_name'].widget.attrs['autofocus'] = True
-    for field in self.fields:
-        if self.fields[field].required:
-            placeholder = f'{placeholders[field]} *'
-        else:
-            placeholder = placeholders[field]
-        self.fields[field].widget.attrs['placeholder'] = placeholder
-        self.fields[field].widget.attrs['class'] = 'stripe-style-input'
-        self.fields[field].label = False
+        self.fields['first_name'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if field != 'country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            self.fields[field].label = False
