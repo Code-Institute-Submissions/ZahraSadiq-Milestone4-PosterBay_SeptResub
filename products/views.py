be0a465be2692c999ceaee1bead47c7f09867aaf
ZahraSadiq <zsadiq27@gmail.com>
2021-04-10 09:42:41 +0000

    Tried to make category filter work

--- products/views.py
+++ products/views.py
@@ -14,7 +14,7 @@ def all_products(request):
 
     products = Product.objects.all()
     query = None
-    category = None
+    categories = None
 
     if request.GET:
         if 'categories' in request.GET:
