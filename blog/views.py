95682fcf70408b4ec0520191f90f3a06db029803
ZahraSadiq <zsadiq27@gmail.com>
2021-09-02 18:59:31 +0000

    Added success & error messages to blog/views.py file so that when a new blog is created the user knows whether it succeeded or failed to upload

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.views import generic
 from .models import Post 
 from .forms import PostForm
@@ -20,16 +21,23 @@ class PostDetail(generic.DetailView):
 
 
 # Add a new blog post to Blog page
+# Help from Master Code Online
 def add_post(request):
     """ Add a new blog post """
     form = PostForm(request.POST or None)
     template = 'add_post.html'
     
-    if form.is_valid():
-        form.save()
+    try:
+        if form.is_valid():
+            form.save()
+            messages.success(request,
+            f'Yahoo! A new blog post has been uploaded to the database')
 
-    else:
+            return redirect(reverse('blog'))
+
+    except Exception as e:
         form = PostForm()
+        messages.warning(request, 'Blog post was not saved. Error: {}'.format(e))
 
     context = {
         'form': form,
